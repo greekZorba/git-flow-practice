9d25c938f06edda6e919a98e7e4cef2e4cdfe0e7
jinhak Kim <smiling.jinhak@gmail.com>
2019-10-20 17:41:48 +0900

    [feature/mainView] 메인 html 띄우기

--- src/main/kotlin/com/example/demo/FirstController.kt
+++ src/main/kotlin/com/example/demo/FirstController.kt
@@ -1,13 +1,13 @@
 package com.example.demo
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 
-@RestController
+@Controller
 class FirstController {
 
     @GetMapping("/")
-    fun welcomeMain(): Mono<String> = "hello webflux".toMono()
+    fun welcomeMain(): Mono<String> = "main".toMono()
 }
